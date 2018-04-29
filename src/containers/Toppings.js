87b5a8da62aa7af424dabced431d11c442406603
laviniaeleanor <laviniaeleanor@outlook.com>
2018-04-29 11:38:03 +0200

    Maximum three toppings in state before submission, still need to remove unchecked items

--- src/containers/Toppings.js
+++ src/containers/Toppings.js
@@ -2,34 +2,8 @@ import React, { Component } from 'react';
 import {pineapple, corn, olives, redOnion, spinach, cherryTomatoes, chicken} from '../data/menu'
 import Paper from 'material-ui/Paper';
 
-function limit(limit) {
-    const elements = document.getElementsByClassName('limited')
-    console.log(elements)
-
-    const filteredElements = elements.filter((x) => {return x.checked})
-    if (filteredElements.length > limit) {alert('Please select no more than ' + limit + ' checkboxes.')
-    return false}
-    else return true
-}
-
-function limitchex(oCheckbox, limit)
-{
-    var el, i = 0, n = limit, oForm = oCheckbox.form;
-    while (el === oForm.elements[i++])
-    {
-        if (el.className === 'limited' && el.checked)
-        --n;
-        if (n < 0)
-        {
-            alert('Please select no more than ' + limit + ' checkboxes.')
-            return false;
-        }
-    }
-    return true;
-}
-
 export class Toppings extends Component {
-  state = {toppings: {}}
+  state = {toppings: []}
 
   checkItem = (item, e) => {
       let toppings = this.state.toppings
@@ -39,14 +13,19 @@ export class Toppings extends Component {
 
   handleChange = (e) => {
         console.log(this.state)
-        const newTopping = {name: e.target.name, price: e.target.value}
-        console.log(newTopping)
+        const newTopping = e.target.name
+        console.log(e.target.checked)
+        console.log(e.target.name)
+        console.log(this.state.toppings)
+        if (e.target.checked === true)
         this.setState({toppings: [...this.state.toppings, newTopping]})
+        else this.setState(this.state.toppings.filter(name => name !== e.target.name))
+        // console.log(Object.values(this.state.toppings).map(array => array.name))
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
-        // limit(3)
+        if (this.state.toppings.length > 3) return alert("Please only choose two toppings")
         this.props.onSubmit(this.state.toppings)
         console.log('You have selected:', this.state.toppings);
 }
@@ -57,19 +36,19 @@ export class Toppings extends Component {
             <form name = "toppings" onSubmit={this.handleSubmit}>
             <h2> Choose up to three toppings:</h2>
 
-              <input type="checkbox" className="limited" name= {pineapple.name} value={pineapple.price} onChange={this.handleChange} />{pineapple.name}<br></br>
+              <input type="checkbox" className="limited" name= {pineapple.name} value={pineapple.name} onChange={this.handleChange}/>{pineapple.name}<br></br>
 
-              <input type="checkbox" className="limited" name= {corn.name} value={corn.price} onChange={this.handleChange} />{corn.name}<br></br>
+              <input type="checkbox" className="limited" name= {corn.name} value={corn.name} onChange={this.handleChange} />{corn.name}<br></br>
 
-              <input type="checkbox" className="limited" name= {olives.name} value={olives.price} onChange={this.handleChange} />{olives.name}<br></br>
+              <input type="checkbox" className="limited" name= {olives.name} value={olives.name} onChange={this.handleChange} />{olives.name}<br></br>
 
-              <input type="checkbox" className="limited" name= {redOnion.name} value={redOnion.price} onChange={this.handleChange} />{redOnion.name}<br></br>
+              <input type="checkbox" className="limited" name= {redOnion.name} value={redOnion.name} onChange={this.handleChange} />{redOnion.name}<br></br>
 
-              <input type="checkbox" className="limited" name= {spinach.name} value={spinach.price} onChange={this.handleChange} />{spinach.name}<br></br>
+              <input type="checkbox" className="limited" name= {spinach.name} value={spinach.name} onChange={this.handleChange} />{spinach.name}<br></br>
 
-              <input type="checkbox" className="limited" name= {cherryTomatoes.name} value={cherryTomatoes.price} onChange={this.handleChange} />{cherryTomatoes.name}<br></br>
+              <input type="checkbox" className="limited" name= {cherryTomatoes.name} value={cherryTomatoes.name} onChange={this.handleChange} />{cherryTomatoes.name}<br></br>
 
-              <input type="checkbox" className="limited" name= {chicken.name} value={chicken.price} onChange={this.handleChange} />{chicken.name}<br></br>
+              <input type="checkbox" className="limited" name= {chicken.name} value={chicken.name} onChange={this.handleChange} />{chicken.name}<br></br>
 
               <button className="btn btn-default" type="submit">Save</button>
             </form>
